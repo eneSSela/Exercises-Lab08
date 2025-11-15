56175b098eb3b404b8940ad79614deea3398016a
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-15 21:32:03 +0100

    Implement testWriteDeathCause

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,6 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,8 +15,12 @@ class TestDeathNote {
 
     private static final String ALICE = "Alice";
     private static final String BOB = "Bob";
+    private static final String CAR_ACCIDENT = "car accident";
+    private static final String HEART_ATTACK = "heart attack";
+    private static final String KARTING_ACCIDENT = "karting accident";
     private static final int ZERO = 0;
     private static final int NEGATIVE_NUMBER = -1;
+    private static final int DEATH_CAUSE_TIME = 100;
 
     @Test
     void testInvalidNegativeRule() {
@@ -58,5 +63,34 @@ class TestDeathNote {
         assertTrue(notes.isNameWritten(ALICE));
         assertFalse(notes.isNameWritten(BOB));
         assertFalse(notes.isNameWritten(""));
+    }
+
+    @Test 
+    void testWriteDeathCause() {
+        final DeathNote notes = new DeathNoteImpl();
+        try {
+            notes.writeDeathCause(CAR_ACCIDENT);
+            fail("Expected IllegalStateArgument");
+        } catch (final IllegalStateException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isEmpty());
+            assertFalse(e.getMessage().isBlank());
+        }
+
+        notes.writeName(ALICE);
+        assertEquals(HEART_ATTACK, notes.getDeathCause(ALICE));
+
+        notes.writeName(BOB);
+        assertTrue(notes.writeDeathCause(KARTING_ACCIDENT));
+        assertEquals(KARTING_ACCIDENT, notes.getDeathCause(BOB));
+
+        try {
+            Thread.sleep(DEATH_CAUSE_TIME);
+        } catch (final InterruptedException e) {
+            fail("Thread.Sleep was interrupted...\n");
+        }
+
+        assertFalse(notes.writeDeathCause(CAR_ACCIDENT));
+        assertEquals(KARTING_ACCIDENT, notes.getDeathCause(BOB));
     }
 }
