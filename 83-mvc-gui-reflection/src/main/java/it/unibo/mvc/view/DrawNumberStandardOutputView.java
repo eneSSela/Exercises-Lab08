b552df35a38332084b96b28e73237ec64b9b569b
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-15 23:05:31 +0100

    Modify DrawNumberStandardOutputView Class

---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStandardOutputView.java
@@ -0,0 +1,42 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawResult;
+
+/**
+ * This class implements a view that can write on any PrintStream.
+ */
+public final class DrawNumberStandardOutputView implements DrawNumberView {
+
+    /**
+     * Builds a new {@link DrawNumberStandardOutputView}.
+     */
+    public DrawNumberStandardOutputView() {
+        /*
+         * Constructor present to comply with Java's requirement of documenting the default constructor.
+         */
+    }
+
+    @Override
+    public void setController(final DrawNumberController observer) {
+        /*
+         * This view does not handle input, so we do not store the controller.
+         */
+    }
+
+    @Override
+    public void start() {
+        /*
+         * Nothing to initialize for console output; ready to print results.
+         */
+    }
+
+    @Override
+    public void result(final DrawResult res) {
+        /*
+         * Print the result description to the standard output.
+         */
+        System.out.println(res.getDescription());
+    }
+}
